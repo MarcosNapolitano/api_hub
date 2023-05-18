9f382d24016e5bc5d7e2f76dd4717ee1f74972b6
unknown <marcosnapolitano@hotmail.com.ar>
2023-05-18 13:56:09 -0300

    added url api

---
+++ Logic/urlShortener.js
@@ -0,0 +1,46 @@
+export const checkUrl = (url) => {
+
+    //gets url and trims
+    const RE = /^https?:\/\//
+
+    //test for URL 
+    if(!(RE.test(url))){
+      
+      return false
+    }
+
+    return true
+}
+
+let data = new Map()
+let actual_id = -1;
+
+export const postUrl = (url, res) => {
+
+    //search in map object
+    for (const [key, value] of data.entries()) {
+
+        if (value == url){
+            return res.json({url: url, short_url: key});
+        }
+    }
+    
+    //if not in database insert
+    actual_id++
+    data.set(actual_id,url)
+  
+    return res.json({url: url, short_url: actual_id});
+
+}
+
+export const getUrl = (id, res) => {
+
+    //get id and search if it exists in the list
+    const LINK = data.get(parseInt(id));
+    //if exists redirect else show err
+    if(LINK) {
+      return res.redirect(LINK);
+    } else {
+      return res.json({error: 'url not inserted'});
+    }
+}
--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,34 @@
 
 import pkg from 'body-parser';
 const { urlencoded } = pkg;
+
+//metric api
 import ConvertHandler from '../Logic/convertHandler.js';
+
+//urlshortener api
+import {checkUrl, postUrl, getUrl} from '../Logic/urlShortener.js';
 
 export default function (app) {
 
+  //for post request
+  app.use(urlencoded({ extended: false }));
+
+  //metric api "index"
+  app.route("/api/convert")
+    .get(function(req,res){
+      res.sendFile(process.cwd() + '/views/metric.html');
+  })
+
+  //urlshortener api "index"
+  app.route("/api/urlshortener")
+    .get(function(req,res){
+      res.sendFile(process.cwd() + '/views/urlshortener.html');
+  })
+
+
+
+
+  //metric api object creation 
   const implement = function(query, res){
 
     const convert = new ConvertHandler(query,res);
@@ -25,19 +49,41 @@ export default function (app) {
               returnUnit: convert.returnUnit, 
               string: convert.getString()})
   }
-
-  app.use(urlencoded({ extended: false }));
-  
-  app.route('/api/convert').get(function(req,res){
+  //metric api
+  app.route('/api/convert/convert').get(function(req,res){
 
     //stores and separates unit from number
     const query = req.query.input
     implement(query,res)
     
-  }).post(function(req,res){
+  })
 
-    const query = req.body.input
-    implement(query, res)
+
+
+  // hello api
+  app.get('/api/hello', function(req, res) {
+    res.json({ greeting: 'hello API' });
+  });
+
+
+
+  //url shortener api
+  app.post('/api/urlshortener', function(req, res) {
+
+    const ORIGINAL_URL = req.body.url;
+
+    if(!checkUrl(ORIGINAL_URL)){
+      return res.json({"error": "invalid URL"});
+    }
+
+    return postUrl(ORIGINAL_URL,res)
+    
+  });
+
+  app.get('/api/urlshortener/:id', (req, res) => {
+
+    const ID = req.params.id;
+    return getUrl(ID, res);
 
   })
 
